b641a0b1f829bee26b392e1e9bd05e55723d86a8
mad <Matthew@collaborationspharma.com>
2023-07-07 16:52:32 -0400

    Started writing program to generate uniform superpositions, still debugging.

--- sandbox/MAD/test_qulacs.py
+++ sandbox/MAD/test_qulacs.py
@@ -1,5 +1,40 @@
 import sys, os
+import numpy as np
 import qulacs 
+from qulacs import QuantumState
+from qulacs import QuantumCircuit
+from qulacs.gate import H
+from qulacs import Observable
 
-print(f"\nqualcs has successfully been installed!")
+## parameters ## 
+verbose = True
 
+# generate n qubit stat
+n = 5
+
+if verbose:
+	print(f"\nGenerating {n}-qubit state.")
+
+# initialize state of qubits
+state = QuantumState(n)
+state.set_zero_state()
+
+# build quantum circuit
+circuit = QuantumCircuit(n)
+for i in range(n):
+	# add Hadamar gate to each wire
+	gate = H(i)
+	print(gate)
+	circuit.add_gate(gate)
+print(circuit)
+
+# apply hadamar gate to all bits
+# generating a uniform super position
+circuit.update_quantum_state(state)
+
+# measure the distribution by sampling
+# is it even?
+obs = Observable(n)
+data = obs.get_expectation_value(state)
+print(data)
+# print(state.get_vector())
