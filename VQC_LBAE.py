01608f18e33e0673332e46cd855627ec3837108c
Matthew Dorsey <madorse2@ncsu.edu>
2024-01-05 13:37:33 -0500

    Added file for uploading files to NCSU cluster (HAZEL). Adjusted VQC to load LBAE data, which has already been fingerprinted.

--- VQC_LBAE.py
+++ VQC_LBAE.py
@@ -6,6 +6,6 @@ from fujitsu.VQC.variational_quantum_classifier import VQC, optimize
 vqc = VQC(qubits = 28)
 
 # load the LABE data
-vqc.load_data(load_path = './fujitsu/data/LBAE/plasmodium_malaria_subsampled_encoded_28_train.csv',\
-	class_col = 'binary', vec_col = 'Encoded_Bit_Vector')
+vqc.load_vector_data(load_path = './fujitsu/data/LBAE/plasmodium_malaria_subsampled_encoded_28_train.csv',\
+	class_col = 'binary', vec_col = 'Encoded_Bit_Vector', verbose = True)
 # TODO :: load bit vector from column data
